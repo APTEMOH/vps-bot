0d0fb737c8788cad7d9e63ab6c72f55df2fe3f17
APTEMOH <ArtIncProject@yandex.ru>
2018-09-25 00:41:30 +0300

    token
    token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api')
 const os = require('os')
 
-const bot = new TelegramBot('554225841:AAFzAjcxEN-Ki2d2xjeROtBbynG8qAjuGeI', {polling: true})
+const bot = new TelegramBot('5***1:A***I', {polling: true})
 
 bot.on('message', msg => {
 	bot.sendMessage(msg.chat.id, `Hello from ${os.type()}, bot says hi to "${msg.from.first_name}"`)
